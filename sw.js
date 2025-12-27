a3cd7b48d69ec15228f2b44fd126a2344e4b3325
akfsilva <alinekfs@protonmail.com>
2025-12-27 13:43:28 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,26 @@
-const CACHE_NAME = 'vault-tec-v1';
+const CACHE_NAME = "vault-v2.5";
 const ASSETS = [
-  './',
-  './index.html',
-  './app.js',
-  './icon-192.png',
-  './icon-512.png',
-  './manifest.json'
+  "index.html",
+  "style.css",
+  "app.js",
+  "manifest.json",
+  "https://raw.githubusercontent.com/Fortis-Solutions/Vault-Stock/main/icon-192.png"
 ];
 
-self.addEventListener('install', event => {
+// Instalação do cache
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
   );
 });
 
-self.addEventListener('fetch', event => {
+// Estratégia: Tenta Rede, se falhar, vai no Cache
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
     })
   );
-}
-                     );
+});
